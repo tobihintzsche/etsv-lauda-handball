c277d19d179f7bba26043c18d8a5da4338ef4e17
Tobias Hintzsche <tobihintzsche2000@gmail.com>
2022-02-24 18:13:48 +0100

    overwrite herren

--- pages/herren.tsx
+++ pages/herren.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-import Platzhalter from '../images/Platzhalter.png'
 
 export default function herren() {
-  return (
-    <div className="">
-      <h2 className="text-center text-xl  font-bold">Herren</h2>
-      <div>
-        <Image src={Platzhalter} height={150} width={230} alt="Headline" />
-      </div>
+  return <div className=''>
+    <h2 className="text-center text-xl  font-bold">Herren</h2>
+    <div>
     </div>
-  )
+    </div>
 }
