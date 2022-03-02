7c964a7c97178e8f69b3755cc7e3ccfd03e50263
Tobias Hintzsche <tobihintzsche2000@gmail.com>
2022-03-02 19:35:13 +0100

    add graphql

---
+++ components/beitragSmall.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface IProps {
+  title: string
+  date: string
+  description: string
+  image: string
+}
+
+const BeitragComponentSmall = ({ title, date, description, image }: IProps) => {
+  return (
+    <main>
+      <div className="bg-neutral-400/50 mt-3 mb-3 mx-auto rounded-md border-2 border-black">
+        <div className="w-4/5 pt-3 pb-3 mx-auto">
+          <h1 className="text-xl font-bold ">{title}</h1>
+          <h2 className="text-sm font-light text-right">{date}</h2>
+          <p>{description}</p>
+
+          <div className="flex flex-col  md:flex-row justify-between ">
+            <div className="w-full pt-4 md:w-full">
+              <img src={image} alt={title} className="rounded-md" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default BeitragComponentSmall
--- pages/herren.tsx
+++ pages/herren.tsx
@@ -9,10 +9,26 @@ import { MDXRemote } from 'next-mdx-remote'
 import Beitrag from './events/[slug]'
 
 export default function herren() {
+
+  const query = gql`query beitrag {
+    beitraege(where: {zuweisung:"Herren"}) {
+      slug
+      title
+      description
+    }
+  }`
+
+  const endpoint = 'https://api-eu-central-1.graphcms.com/v2/cl0874wb42pah01xr1jnmabfu/master'
+
+  const client = new GraphQLClient(endpoint, { headers: {} })
+  client.request(query).then((data) => console.log(data))
+
+  const digits = [1,2,3]
+
   return (
     <div>
       <h1>Herren</h1>
-      {}
+  {}
     </div>
   )
 }
