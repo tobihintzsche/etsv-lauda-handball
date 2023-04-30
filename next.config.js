5a92a15127d8f4cd8693f7f40b2121455d0d43c4
Tobias Hintzsche <tobias.hintzsche@dertouristik.com>
2023-04-30 22:18:07 +0200

    adjust next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    async headers() {
+      return [
+        {
+          source: '/(.*)?',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            },
+          ],
+        },
+      ]
+    },
+  },
 }
 
 module.exports = nextConfig
